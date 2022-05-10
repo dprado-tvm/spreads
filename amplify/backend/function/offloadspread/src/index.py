2c764d04bc0d8072c16e473a6e71789d6170a002
Deblyn Prado <deblynprado@hotmail.com>
2022-05-10 15:07:47 -0300

    Getting data from dynamodb

--- amplify/backend/function/offloadspread/src/index.py
+++ amplify/backend/function/offloadspread/src/index.py
@@ -1,30 +1,18 @@
 import json
 import boto3
+from boto3.dynamodb.conditions import Key
 
 client = boto3.client('dynamodb', region_name='us-west-2')
 
-def handler(event, context):
-  data = client.query(
-    TableName='TVSpreads',
-    IndexName='Symbol',
-    KeyConditionExpression='#name = :value',
-    ExpressionAttributeValues={
-      ':value': {
-        'S': 'shoes'
-      }
-    },
-    ExpressionAttributeNames={
-      '#name': 'name'
-    }
-  )
+def handler(event, context): 
+  def query():
+    dynamodb = boto3.resource('dynamodb')
 
-  response = {
-      'statusCode': 200,
-      'body': json.dumps(data),
-      'headers': {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*'
-      },
-  }
-  
-  return response
+    table = dynamodb.Table('TVSpreads')      
+
+    resp = table.query(
+      KeyConditionExpression=Key('Symbol').eq('SPXm')
+    )
+    
+    for item in resp['Items']:
+      print(item)
